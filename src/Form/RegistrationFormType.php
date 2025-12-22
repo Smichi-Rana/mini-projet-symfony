fb9069478ab7a79337748a1d79f6c91ddb14fd3b
Rana <smichirana4@gmail.com>
2025-12-22 17:26:17 +0100

    atelier5-Authenticator

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Client' => 'ROLE_CLIENT',
+                    'Agent'  => 'ROLE_AGENT',
+                ],
+                'expanded' => true,   // radio buttons
+                'multiple' => false,  // rôle واحد فقط
+                'label' => 'Choisissez un rôle',
+            ])
+
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+
+        // ✅ TRANSFORMER pour roles (string <-> array)
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray): ?string {
+                    return $rolesArray[0] ?? null;
+                },
+                function ($roleString): array {
+                    return [$roleString];
+                }
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
